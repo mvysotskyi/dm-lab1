6bc2811349e0badc7f84f86bc38d50c6d352e436
ayrinnnx <117468608+ayrinnnx@users.noreply.github.com>
2023-02-27 23:59:32 +0200

    Added docstrings + sorting of final result.

--- kruskal_prim/kruskal_prim.py
+++ kruskal_prim/kruskal_prim.py
@@ -4,13 +4,24 @@ Kruskal and Prim algorithms.
 
 from graph_generator import gnp_random_connected_graph
 
-def kruskal_search(graph):
-    edges = sorted(graph.edges(data=True), key = lambda x: x[2]['weight'])
+def kruskal_search(graph) -> list:
+    """
+    Implements the Kruskal's algorithm to find
+    the minimum spanning tree of a graph.
+    Returns list[tuple], where tuple is tuple of nodes.
+    >>> import networkx as nx
+    >>> G = nx.Graph()
+    >>> G.add_edge(1, 2, weight=2)
+    >>> G.add_edge(1, 3, weight=3)
+    >>> G.add_edge(2, 3, weight=1)
+    >>> kruskal_search(G)
+    [(1, 2), (2, 3)]
+    """
+    edges = sorted(graph.edges(data=True), key = lambda x: x[2]['weight']) 
     #list of sorted weights
     points = [{i} for i in list(graph.nodes())]
     result = []
-    ind1 = None
-    ind2 = None
+    ind1, ind2 = None, None
     for edge in edges:
         for point in points:
             if edge[0] in point:
@@ -21,4 +32,4 @@ def kruskal_search(graph):
             points[ind1] = points[ind1].union(points[ind2])
             del points[ind2]
             result.append((edge[0], edge[1]))   
-    return result
+    return sorted(result, key = lambda x: x[0])
