ad1ede55f1b7e4ac84592f8bdc4fd4e465d622c6
ayrinnnx <117468608+ayrinnnx@users.noreply.github.com>
2023-02-28 02:13:56 +0200

    Added experiment of comparing diff kruskal codes.

--- kruskal_prim/kruskal_prim.py
+++ kruskal_prim/kruskal_prim.py
@@ -33,3 +33,45 @@ def kruskal_search(graph) -> list:
             del points[ind2]
             result.append((edge[0], edge[1]))   
     return sorted(result, key = lambda x: x[0])
+
+
+def timing(values):
+    random_fullfil = [random.random() for i in range(49)]
+    iterations = 1000
+    time_taken = 0
+    nx_time_taken = 0
+    list_time_taken = []
+    nx_list_time_taken = []
+    for i in range(len(values)):
+        print(i)
+        graph = gnp_random_connected_graph(values[i], random_fullfil[i], False, False)
+        for j in range(iterations):
+            start = time.time()
+            tree.minimum_spanning_tree(graph, algorithm="kruskal")
+            end = time.time()
+            nx_time_taken += end - start
+            start = time.time()
+            kruskal_search(graph)
+            end = time.time()
+            time_taken += end - start
+        list_time_taken.append(time_taken/iterations)
+        nx_list_time_taken.append(nx_time_taken/iterations)
+    return list_time_taken, nx_list_time_taken
+
+
+if __name__ == '__main__':
+    import doctest
+    print(doctest.testmod())
+    values = range(10, 500, 10)
+    kruskal_times, nx_times = timing(values)
+    print(f'Minimal value of written algorithm: {min(kruskal_times)}, \
+            Maximum value: {max(kruskal_times)}')
+    print(f'Minimal value of implemented algorithm: {min(nx_times)}, \
+            Maximum value: {max(nx_times)}')
+    plt.plot(values, kruskal_times, label="Written")
+    plt.plot(values, nx_times, label="Nx")
+    plt.legend()
+    plt.xlabel("Graph size")
+    plt.ylabel("Time (seconds)")
+    plt.show()
+
